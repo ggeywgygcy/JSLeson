13c2bc331484dd839c9725e563536e7398b323df
Lenya <wasilij18bus@gmail.com>
2023-12-16 18:08:16 +0300

    ура, столько мучений

--- script.js
+++ script.js
@@ -1,57 +1,40 @@
-//let condition = false;
-//console.log(userName);
-//if(userName == "Путиин"){
-   // alert('Ты стал путиным')
-//}else{
-    //alert('Я не хочу')
-//}
-//switch(userName){
-  //  case 'Путин':
-    //    alert('Я не хочу ыв тюрбму, отправьте Кирилла');
-      //  break;
-    //case// 'Бфйдлен':
-        //al//ert('Когда ты сдохнешь');
-        //break
-    //case 'Шарик':
-      //  alert('Че за скинволкер за компьютеп');
-    //case 'Kirill':
-        //alert('fbjkbfbkfbjkbkjfbkwb');
-        //break;
-    //default :
-      //  alert('nkjvbuebvkebikfb');    
-//}
-// for(let i=0; i < 10; i++){
-//     console.log(i * 5)
-// }
+let btn = document.getElementById('btn');
+let temp = document.getElementById('fnode');
+let canv = document.getElementById('canv')
+btn.addEventListener('click',loadFoto);
 
-// let userName = prompt('Введите имя свое');
-// while(userName != 'q'){
-//     console.log(userName)
-//     userName = prompt("Чтобы выйти нажмите й");
-//     alert('Посмотри на меня быстро');
-//     if(userName == 2){
-//         alert('Продолжаем введено 2');
-//         continue;
-//     }
-    
-//     alert('не отображается если 1')
-//     if(userName == 1){
-//         alert('Вываливаемся из цикла! Все кончено, гипс снимают');
-//         break;
-//     }
-// }
-// let ar = ['кирил','максик','артурик'];
-// console.log(ar.length);
-// ar.push('Темик');
-// console.table(ar);
 
-let inputmany = document.getElementById('many').value;
-let checkMany = document.getElementById('chekMany').value;
-let out = document.getElementById('out');
+function loadFoto(rangeFoto){
+    fetch('https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=10').then(result => result.json())
+      .then(res => res.photos).then(a =>{
+        let arr=[];
+        for(let i=0; i < 10; i++){
+          arr.push(a[i].url);
+        }
+        //console.log(arr)
+        return arr;
+        }
+        ).then(b =>{
+          //console.log(b);
+          //createElement(b);
+          for(let i = 0; i < 10; i++){
+            let foto = temp.content.cloneNode(true);
+             foto.getElementById('ft').setAttribute('src', b[i])
+             console.log(b[i]);
+             canv.appendChild(foto);
+            }
+        } );
 
-let ar = [82, 96, 12, 0.6]
 
-function converter(){
-    let res = inputmany * ar[checkMany -1];
-    out.innerText = `Стоимость по курсу ${res}`;
+
 }
+
+
+// function createElement(l){
+//   for(let i = 0; i < l.lenght; i++){
+//   let foto = temp.content.cloneNode(true);
+//    foto.getElementById('ft').setAttribute('src', l[i])
+//    console.log(l[i]);
+//    canv.appendChild(foto);
+//   }
+// }

bdee6c51b22e2e6f97c86ded1bf4df867aa342a8
Lenya <wasilij18bus@gmail.com>
2023-12-23 17:56:41 +0300

    арбитраж

--- script.js
+++ script.js
@@ -1,57 +1,141 @@
-//let condition = false;
-//console.log(userName);
-//if(userName == "Путиин"){
-   // alert('Ты стал путиным')
-//}else{
-    //alert('Я не хочу')
-//}
-//switch(userName){
-  //  case 'Путин':
-    //    alert('Я не хочу ыв тюрбму, отправьте Кирилла');
-      //  break;
-    //case// 'Бфйдлен':
-        //al//ert('Когда ты сдохнешь');
-        //break
-    //case 'Шарик':
-      //  alert('Че за скинволкер за компьютеп');
-    //case 'Kirill':
-        //alert('fbjkbfbkfbjkbkjfbkwb');
-        //break;
-    //default :
-      //  alert('nkjvbuebvkebikfb');    
-//}
-// for(let i=0; i < 10; i++){
-//     console.log(i * 5)
+// // function test(a){
+  
+// //   console.log(arguments);
+// // }
+
+// // test(5, 7, "juj");
+
+// let dogs = ["Шарик", "Бобик", "Полкан"];
+
+// [sharik, bob] = dogs;
+
+// console.log(sharik, bob);
+
+// [bob, sharik] = [sharik, bob]
+// console.log(sharik, bob);
+
+// let car = {
+//   weel: "19d",
+//   acselerator: "100",
+//   color: "red"
 // }
 
-// let userName = prompt('Введите имя свое');
-// while(userName != 'q'){
-//     console.log(userName)
-//     userName = prompt("Чтобы выйти нажмите й");
-//     alert('Посмотри на меня быстро');
-//     if(userName == 2){
-//         alert('Продолжаем введено 2');
-//         continue;
-//     }
-    
-//     alert('не отображается если 1')
-//     if(userName == 1){
-//         alert('Вываливаемся из цикла! Все кончено, гипс снимают');
-//         break;
-//     }
+// let {color: r, weel} = car;
+// console.log(r, weel);
+
+// let limonad = {
+//   sirop: "Ваниль",
+//   obiem: "7liter",
+//   conservators: "rapi"
 // }
-// let ar = ['кирил','максик','артурик'];
-// console.log(ar.length);
-// ar.push('Темик');
-// console.table(ar);
 
-let inputmany = document.getElementById('many').value;
-let checkMany = document.getElementById('chekMany').value;
-let out = document.getElementById('out');
+// let svetLimonad = {
+//   sirop: "Ваниль",
+//   obiem: "7liter",
+//   conservators: "no"
+// }
 
-let ar = [82, 96, 12, 0.6]
+// let a = 2;
+// let b ;
+// b = a;
+// console.log(`a = ${a} b=${b}`);
 
-function converter(){
-    let res = inputmany * ar[checkMany -1];
-    out.innerText = `Стоимость по курсу ${res}`;
+// b = 1;
+// console.log(`a = ${a} b=${b}`);
+
+// limonad = svetLimonad;
+// console.table(limonad);
+// limonad.conservators = "ref";
+// console.log('new limonad')
+// console.table(Limonad)
+// console.log("ussr limonad")
+// console.table(svetLimonad)
+
+// limonad = Object.assign(svetLimonad)
+// console.log('new limonad')
+// console.table(limonad)
+// console.log("ussr limonad")
+// console.table(svetLimonad)
+
+// limonad.conservators = "ref";
+
+// console.log('new limonad')
+// console.table(limonad)
+// console.log("ussr limonad")
+// console.table(svetLimonad)
+
+
+// const Dogs =function(age, nAme, weight, poroda){
+//     this.age = age;
+//     this.nAme = nAme;
+//     this.weight = weight;
+//     this.poroda = poroda;
+//     this.speek = function(){
+//       console.log("[fd [fd")
+//     }
+
+// }
+
+// Dogs.prototype.speek = function(){
+//   console.log("[fd [fd")
+// }
+
+
+// class Dogs {
+//   age 
+//   nAme
+//   weight
+//   poroda
+
+
+//   constructor(age, nAme, weight, poroda){
+//     this.age = age;
+//      this.nAme = nAme;
+//      this.weight = weight;
+//      this.poroda = poroda;
+//   }
+//   speek(){
+//     console.log("[fd [fd")
+//   }
+// }
+
+// let sharik = new Dogs(2, "Шарон", 34, "Ровный")
+// let babon = new Dogs(3, "Бабоха", 45, "people")
+// babon.age = 1
+
+// console.table(sharik)
+// console.table(babon)
+// sharik.speek();
+// babon.speek();
+
+
+class Staff{
+  #userName;
+  #age;
+  constructor(uname, age){
+    this.#userName = uname;
+    this.#age = age;
+  }
+
+  getUname(){
+    console.log(this.#userName);
+  }
 }
+
+class Manager extends Staff{
+    valueSale;
+    constructor(uname, age, sale){
+        super(uname, age);
+        this.valueSale = 10;
+    }
+    resultSale(){
+      console.log(this.valueSale)
+    }
+}
+
+
+
+let semen = new Manager('Семен', 24, 10);
+
+semen.getUname();
+
